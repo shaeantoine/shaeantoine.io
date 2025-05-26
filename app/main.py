40fc272ff14d1a9413029a6788ccc8dafc603f0c
smck0583 <smck0583@uni.sydney.edu.au>
2025-05-26 20:35:12 +1000

    Finalizing changes for Render publishing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI() 
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
