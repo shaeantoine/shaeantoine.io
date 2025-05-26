61f03044ac93b02a3c15166fb3940bf1ad13b121
smck0583 <smck0583@uni.sydney.edu.au>
2025-05-26 20:10:17 +1000

    adding to main.py for 2 basic endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from fastapi import * 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI() 
 
-@app.get("/")
-def main(): 
-    return {"Hello": "World!"}
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def landing(request: Request): 
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request): 
+    return templates.TemplateResponse("about.html", {"request": request})
